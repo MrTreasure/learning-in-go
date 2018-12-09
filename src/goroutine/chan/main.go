7bc9d6c6a843f1a4114c18cee80da6e3949c7c6b
Treasure <treasurewmj@foxmail.com>
2018-12-09 16:34:53 +0800

    feat:多协程完成任务

--- src/goroutine/chan/main.go
+++ src/goroutine/chan/main.go
@@ -6,11 +6,34 @@ import (
 )
 
 func main() {
-	intChan := make(chan int)
-	go write(intChan)
-	go read(intChan)
+	intChan := make(chan int, 1000)
+	resultChan := make(chan int, 1000)
 
-	time.Sleep(10 * time.Second)
+	for i := 0; i < 100000; i++ {
+		intChan <- i
+	}
+
+	for i := 0; i < 12; i++ {
+		// chan 是线程安全的管道，不会出现重复取数
+		go calc(intChan, resultChan)
+	}
+
+	time.Sleep(time.Second * 10)
+}
+
+func calc(taskChan chan int, resChan chan int) {
+	for v := range taskChan {
+		flag := true
+		for i := 2; i < v; i++ {
+			if v%i == 0 {
+				flag = false
+				break
+			}
+		}
+		if flag {
+			resChan <- v
+		}
+	}
 }
 
 func read(ch chan int) {
