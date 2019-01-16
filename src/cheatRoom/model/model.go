4ca58a5600a196c976ece44ed1553c7d17a64f42
Treasure <treasurewmj@foxmail.com>
2019-01-16 23:33:00 +0800

    remove:删除测试demo

--- src/cheatRoom/model/model.go
+++
@@ -1,6 +0,0 @@
-package model
-
-type User struct {
-	Name string
-	Age  int
-}
--- src/dispatcher/balance/balance.go
+++
@@ -1,6 +0,0 @@
-package balance
-
-// Balancer 负载均衡
-type Balancer interface {
-	DoBalance([]*Instance) (*Instance, error)
-}
