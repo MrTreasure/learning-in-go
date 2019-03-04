0765ea626b73b3ebc4cfa4e6b721599a74c680ee
Treasure <treasurewmj@foxmail.com>
2019-03-04 23:47:05 +0800

    feat:添加ele thrift 文件

---
+++ src/thrift/ele.thrift
@@ -0,0 +1,17 @@
+namespace go ele.demo
+
+struct Student{
+  1: i32 id,
+  2: string name,
+  3: string duty,
+  4: string department
+}
+
+const map<string, string> MAPCONSTANT = {'name': '饿了么大前端', 'group': 'Alibaba'}
+
+service eleThrift {
+  Student findStuById(1: i32 id),
+  Student findStuByName(1: string name),
+  list<Student> findStuByDuty(1: string duty)
+  void put(1: Student stu)
+}
